70f8b14742bd59c965a22c91841d7c37c0498551
Logan Peterson <lapeterson01@gmail.com>
2018-04-12 13:17:28 -0400

    stub for banking api

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 
+const app = express();
+
 app.use(bodyParser.json());
+
+require('./routes/yodleeRoutes')(app);
 
 app.listen(8000, () => {
     console.log('server is running on port 8000');
